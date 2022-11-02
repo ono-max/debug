bbaed222320368841b63d2a914c2265b9a237fed
Naoto <onoto1998@gmail.com>
2022-11-03 01:00:33 +0900

    fix

--- lib/debug/server_cdp.rb
+++ lib/debug/server_cdp.rb
@@ -233,6 +233,7 @@ module DEBUGGER__
 
       def handshake
         req = @sock.readpartial 4096
+        $stderr.puts @sock.eof?
         show_protocol '>', req
 
         if req.match /^Sec-WebSocket-Key: (.*)\r\n/
