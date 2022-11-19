323e1e7761968d22f1d091f377f7502a72c1e3fe
Naoto Ono <onoto1998@gmail.com>
2022-11-19 20:51:17 +0900

    fix

--- lib/debug/server.rb
+++ lib/debug/server.rb
@@ -277,7 +277,7 @@ module DEBUGGER__
     class NoRemoteError < Exception; end
 
     def sock skip: false
-      sleep 0.05
+      sleep 0.01
       if s = @sock         # already connection
         # ok
       elsif skip == true   # skip process
